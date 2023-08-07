6f18b7619047e769820a64e2f335e895f63bd01f
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-08-07 17:07:12 +0530

    Add files via upload

--- bikerzzone/src/main/java/com/fssa/bikerzzone/dao/ConnectionUtil.java
+++ bikerzzone/src/main/java/com/fssa/bikerzzone/dao/ConnectionUtil.java
@@ -2,43 +2,40 @@ package com.fssa.bikerzzone.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionUtil {
 
 	public static Connection getConnection() {
-
 		Connection con = null;
-		String url = "jdbc:mysql://localhost:3306/bike";
-		String userName = "root";
-		String passWord = "123456";
+
+		String url, userName, passWord;
+
+		if (System.getenv("CI") != null) {
+			url = System.getenv("DATABASE_HOST");
+			userName = System.getenv("DATABASE_USERNAME");
+			passWord = System.getenv("DATABASE_PASSWORD");
+		} else {
+			Dotenv env = Dotenv.load();
+			url = env.get("DATABASE_HOST");
+			userName = env.get("DATABASE_USERNAME");
+			passWord = env.get("DATABASE_PASSWORD");
+		}
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection(url, userName, passWord);
-		} catch (Exception e) { 
+			System.out.println("connection created");
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Unable to connect to the database");
 		}
 		return con;
 	}
-
-	public static void close(Connection conn, Statement stmt, ResultSet rs) {
-
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			// No need re throw the exception.
-		}
+	
+	public static void main(String[] args) {
+		ConnectionUtil.getConnection();
 	}
+	
 }
