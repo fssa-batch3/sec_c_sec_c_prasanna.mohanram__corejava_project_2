be20fcb870e049e58f7d6aac27c6bf6edb0ef44c
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-08-10 10:08:32 +0530

    Update TestBikeService.java

--- src/test/java/com/fssa/bikerzzone/service/TestBikeService.java
+++ src/test/java/com/fssa/bikerzzone/service/TestBikeService.java
@@ -17,7 +17,7 @@ public class TestBikeService {
 
 	@Test
 	 void testAddBike() throws Exception, DAOException {
-		Bike bike = new Bike(1, "Hero", "Splendor160", 80000, "FIRST", "Chennai", LocalDate.of(2003, 9, 5));
+		Bike bike = new Bike(1, "Hero", "Splendor120", 80000, "FIRST", "Chennai", LocalDate.of(2003, 9, 5));
 
 		assertTrue(BikeValidator.validate(bike));
 		assertDoesNotThrow(() -> BikeDao.addBike(bike));
