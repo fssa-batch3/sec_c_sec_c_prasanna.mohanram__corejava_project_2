0d975f8ee198ec2dd3114168bac53b4dd5c36392
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-08-10 09:50:10 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/bikerzzone/logger/Logger.java
@@ -0,0 +1,19 @@
+package com.fssa.bikerzzone.logger;
+
+public class Logger {
+	/**
+	 * Default constructor for creating a Logger object.
+	 */
+	public Logger() {
+		// Default constructor
+	}
+
+	/**
+	 * Print an informational log message to the console.
+	 * 
+	 * @param msg The log message to be printed.
+	 */
+	public static void info(String msg) {
+		System.out.println(msg);
+	}
+}
